c26a0af19627295a2ffee633b6e0d0198674e74a
estif <kingestiff@gmail.com>
2025-02-06 11:07:21 +0300

    Events & states

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,19 @@
-import Message from "./Message";
+import Message, {Button, NewButton, Counter, ToggleButton} from "./Message";    //use brace for button bc its named export not default export like Message
+
 
 function Display(){
-  return <Message/>
+  return (
+    <>
+      <Message/>
+
+      <Button text="Click me" color="red"/>
+      <Button text="Don't click me" color="blue"/>
+
+      <NewButton/>
+      <Counter/>
+      <ToggleButton/>
+    </>
+  )
 }
 
 export default Display;
--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,6 +1,65 @@
+import {useState} from 'react'
+
 function Message(){
-    return <h1>Hello World</h1>
+    var array = ["Cat","Dog", "Rat"];
+    var animalList = array.map((item) => <li key={item}>{item}</li> );       //key is unique identifier here its the Index
+    
+    return(
+        <>
+            {array.length === 0 && <p>No Items inside</p>}
+            <ul>{animalList}</ul>
+        </>
+    ) 
 }
+
+//Properties (Props): in react it prevent us from creating multiple Buttons with differnt texts inside them instead change that dynamically
+// the below parameter are props!! like props.text, props.color we just destructured them for better view
+export function Button({text="Click me", color="green"}){
+    const buttonStyle = {
+        color: color,
+    }
+
+
+    return <button style={buttonStyle}>{text}</button>
+}
+
+// 3. Events
+export function NewButton(){
+    function ButtonEvent(){
+        alert('Subscribed Successfully!!');
+    }
+
+    return <button onClick={ButtonEvent}>Subscribe</button>
+}
+
+//4. States
+export function Counter(){
+    const [count, setCount] = useState(0);      //intialzie count with 0
+
+
+    return(
+        <>
+            <p>{count}</p>
+            <button onClick={()=> setCount(prevCount => prevCount + 1)}>Increase</button>
+            <button onClick={()=> setCount(prevCount => prevCount - 1)}>Decrease</button>
+        </>
+    )
+}
+
+// Project1 (Dark mode Button)
+export function ToggleButton(){
+    const [toggle, setToggle] = useState(false)
+    
+    const ButtonStyle = {
+        color: toggle? "white" : "black",
+        backgroundColor: toggle? "black" : "white",
+        padding: "20px",
+        display: "block"
+    }
+
+    return <button onClick = {() => setToggle(prevToggle => !prevToggle)} style={ButtonStyle}>Toggle</button>
+}
+
 
 
 export default Message;
